40abea11d167408b03584a885863387ac5508978
Olivier Voortman <ov@taktik.com>
2017-01-30 17:08:57 +0100

    Updated to version 0.2.0, cleanups

--- src/main/docker/docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#docker build -t taktik/squirrel-mac-mw .
-docker run -d -t --name transcoder -e SPRING_APPLICATION_NAME=squirrel-mac-mw taktik/squirrel-mac-mw -e MW_NEXUS_SERVER="$NEXUS_SERVER" -e MW_NEXUS_USERNAME="$NEXUS_USERNAME" -e MW_NEXUS_PASSWORD="$NEXUS_PASSWORD" -e MW_NEXUS_REPOSITORY="$NEXUS_REPOSITORY"
